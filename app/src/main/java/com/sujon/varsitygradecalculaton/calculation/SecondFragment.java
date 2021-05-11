cfccf4f43d38a45dcc14edb9a9a87431ddd93800
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-12 03:05:44 +0600

    20_Calculate CGPA - Android Development with Java - Rakibul Islam - Bohubrihi
    20_Calculate CGPA - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/varsitygradecalculaton/calculation/SecondFragment.java
+++ app/src/main/java/com/sujon/varsitygradecalculaton/calculation/SecondFragment.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,23 +19,58 @@ public class SecondFragment extends Fragment {
     View rootview;
     DataController controller;
 
+    EditText creditText,gpaText;
+    Button addButton;
+    TextView cgpaTextView;
+
+    double totalCredit=0;
+    double productofGPAandCredit=0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootview=inflater.inflate(R.layout.fragment_second, container, false);
         controller=DataController.getInstance();
+        creditText = rootview.findViewById(R.id.editTextTextPersonName);
+        gpaText = rootview.findViewById(R.id.editTextTextPersonName2);
+        addButton = rootview.findViewById(R.id.button);
+        cgpaTextView = rootview.findViewById(R.id.textView3);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (creditText.getText().toString().equals("") || gpaText.getText().equals("")) {
+                    Toast.makeText(getContext(), "Insert Credit & GPA", Toast.LENGTH_SHORT).show();
+                } else {
+                    CalculateCGPA(gpaText.getText().toString(), creditText.getText().toString());
+
+                }
+            }
+        });
+
         Toast.makeText(getContext(),controller.getCurrentSemister().getSemisterName(),Toast.LENGTH_SHORT).show();
         return rootview;
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void CalculateCGPA(String gpa,String credit){
+        double gpaValue = Double.parseDouble(gpa);
+        double creditValue = Double.parseDouble(credit);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
+        productofGPAandCredit += (gpaValue * creditValue);
+        totalCredit += creditValue;
+        double cgpa = productofGPAandCredit / totalCredit;
+        cgpaTextView.setText(String.format("CGPA: %.2f",cgpa));
+
     }
+
+//    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(SecondFragment.this)
+//                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+//            }
+//        });
+//    }
 }
